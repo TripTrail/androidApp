3e2b0f08bcb7abc3f39d655e311ee4e135d6b3ba
Ashutosh Meher <ashutoshmeher@Ashutoshs-MacBook-Air.local>
2018-12-12 00:05:28 +0530

    token api refactor

--- app/src/main/java/com/company/application/activity/LoginActivity.java
+++ app/src/main/java/com/company/application/activity/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.company.application;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -12,15 +14,9 @@ import android.widget.Toast;
 
 import com.company.application.model.TokenResponse;
 import com.company.application.util.Constant;
-import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
-
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.util.ArrayList;
-import java.util.List;
 
 public class LoginActivity extends AppCompatActivity{
 
@@ -63,7 +59,15 @@ public class LoginActivity extends AppCompatActivity{
                         if(e==null){
                             if(result.getAccess_token()!=null && !result.getAccess_token().trim().equals("")){
                                 Log.d("TOKEN", result.getAccess_token());
+                                SharedPreferences preferences = getSharedPreferences("TRIPTRAIL_ACTN", Context.MODE_PRIVATE);
+                                preferences.edit()
+                                        .putString("ACCESS_TOKEN", result.getAccess_token())
+                                        .putString("REFRESH_TOKEN", result.getRefresh_token())
+                                        .apply();
                                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                                 startActivity(intent);
                             }else if(result.getStatus()!=null && !result.getStatus()){
                                 Toast.makeText(getBaseContext(), result.getMessage(), Toast.LENGTH_LONG).show();
@@ -72,6 +76,7 @@ public class LoginActivity extends AppCompatActivity{
                                 Toast.makeText(getBaseContext(), "Something Went Wrong!", Toast.LENGTH_LONG).show();
                             }
                         }else{
+                            Log.e("TOKEN ERROR", e.getMessage());
                             Toast.makeText(getBaseContext(), "Something Went Wrong!", Toast.LENGTH_LONG).show();
                         }
 
